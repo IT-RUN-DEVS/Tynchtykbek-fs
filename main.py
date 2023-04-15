4fb87babbf26f5d9b75c7bb113a5cedc50ba2616
user <2004.01012@manas.edu.kg>
2023-04-15 13:04:06 +0600

    вызов функциий через командную строку

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import sys
+import argparse
+import importlib
+import os
 
-if __name__ == '__main__':
-    print('hello')
+def main():
+    commands_dir = os.path.join(os.path.dirname(__file__), 'commands')
+    command_files = [
+        os.path.splitext(f)[0] for f in os.listdir(commands_dir)
+        if os.path.isfile(os.path.join(commands_dir, f)) and f.endswith('.py')
+    ]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", choices=command_files)
+    parser.add_argument("args", nargs="*")
+    args = parser.parse_args()
+
+    module_name = f"commands.{args.command}"
+    module = importlib.import_module(module_name)
+
+    getattr(module, args.command)(*args.args)
+
+
+if __name__ == "__main__":
+    main()
